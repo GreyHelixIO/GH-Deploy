520f3353ebfb9d551897eb0ecf7593af77a226a9
Christopher Smith <csmith4work@gmail.com>
2022-07-12 09:46:57 -0500

    Migrating over tf modules and updating them as necessary.

--- modules/aws/ecs/main.tf
+++ modules/aws/ecs/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_task_definition" "gh_api_task" {
-    family                   = "gh-api-task-${var.env}" # Naming our first task
+    family                   = "gh-api-task-${var.env}"
     container_definitions    = <<DEFINITION
     [
         {
@@ -35,10 +35,10 @@ resource "aws_ecs_task_definition" "gh_api_task" {
         }
     ]
     DEFINITION
-    requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-    network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-    memory                   = 512         # Specifying the memory our container requires
-    cpu                      = 256         # Specifying the CPU our container requires
+    requires_compatibilities = ["FARGATE"]
+    network_mode             = "awsvpc"
+    memory                   = 512
+    cpu                      = 256
     execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
     task_role_arn            = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
@@ -53,61 +53,59 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_cluster" "scp_cluster" {
-    name = "scp-cluster-${var.env}"
+resource "aws_ecs_cluster" "gh_cluster" {
+    name = "gh-cluster-${var.env}"
 }
 
 resource "aws_ecs_service" "gh_api_service" {
-    name            = "gh-api-service-${var.env}"        # Naming our first service
-    cluster         = aws_ecs_cluster.scp_cluster.id           # Referencing our created Cluster
-    task_definition = "${aws_ecs_task_definition.cs_api_task.arn}" # Referencing the task our service will spin up
+    name            = "gh-api-service-${var.env}"
+    cluster         = aws_ecs_cluster.gh_cluster.id
+    task_definition = "${aws_ecs_task_definition.cs_api_task.arn}"
     launch_type     = "FARGATE"
-    desired_count   = 1 # Setting the number of containers we want deployed to 1
+    desired_count   = 1
     deployment_minimum_healthy_percent = 0
     force_new_deployment = true
     network_configuration {
         subnets = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
         assign_public_ip = true
-        security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+        security_groups  = ["${aws_security_group.service_security_group.id}"]
     }
     load_balancer {
-        target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+        target_group_arn = "${aws_lb_target_group.target_group.arn}"
         container_name   = "${aws_ecs_task_definition.cs_api_task.family}"
-        container_port   = 80 # Specifying the container port
+        container_port   = 80
     }
 }
 
 resource "aws_alb" "application_load_balancer" {
-    name               = "gh-alb-${var.env}" # Naming our load balancer
+    name               = "gh-alb-${var.env}"
     load_balancer_type = "application"
-    subnets = [ # Referencing the default subnets
+    subnets = [
         "${aws_default_subnet.default_subnet_a.id}",
         "${aws_default_subnet.default_subnet_b.id}"
     ]
-    # Referencing the security group
+
     security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 }
-
-# Creating a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
     ingress {
-        from_port   = 80 # Allowing traffic in from port 80
+        from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
-        from_port   = 443 # Allowing traffic in from port 80
+        from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
-        from_port   = 0 # Allowing any incoming port
-        to_port     = 0 # Allowing any outgoing port
-        protocol    = "-1" # Allowing any outgoing protocol
-        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
@@ -116,15 +114,14 @@ resource "aws_security_group" "service_security_group" {
         from_port = 0
         to_port   = 0
         protocol  = "-1"
-        # Only allowing traffic in from the load balancer security group
         security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
     }
 
     egress {
-        from_port   = 0 # Allowing any incoming port
-        to_port     = 0 # Allowing any outgoing port
-        protocol    = "-1" # Allowing any outgoing protocol
-        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 resource "aws_lb_target_group" "target_group" {
@@ -132,7 +129,7 @@ resource "aws_lb_target_group" "target_group" {
     port        = 80
     protocol    = "HTTP"
     target_type = "ip"
-    vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+    vpc_id      = "${aws_default_vpc.default_vpc.id}"
     health_check {
         matcher = "200,301,302"
         path = "/"
@@ -141,12 +138,12 @@ resource "aws_lb_target_group" "target_group" {
 }
 
 resource "aws_lb_listener" "listener" {
-    load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+    load_balancer_arn = "${aws_alb.application_load_balancer.arn}"
     port              = "80"
     protocol          = "HTTP"
     default_action {
         type             = "forward"
-        target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+        target_group_arn = "${aws_lb_target_group.target_group.arn}"
     }
 }
 
@@ -159,14 +156,12 @@ resource "aws_cloudwatch_log_group" "log-group" {
     }
 }
 
-# Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" {
     tags = {
         Name = "Default VPC"
     }
 }
 
-# Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
     availability_zone = "us-east-1a"
 }
@@ -175,7 +170,7 @@ resource "aws_default_subnet" "default_subnet_b" {
     availability_zone = "us-east-1b"
 }
 locals {
-    ecs_servcie_secrets = var.env == "prod" ? "arn:aws:secretsmanager:us-east-1:482352589093:secret:prod-gh-config-9QASbT" : "arn:aws:secretsmanager:us-east-1:482352589093:secret:qa-gh-config-bNTQ2q"
+    ecs_servcie_secrets = var.env == "prod" ? "arn:aws:secretsmanager:us-east-1:455667379642:secret:gh-api-config-prod-wvQqls" : "arn:aws:secretsmanager:us-east-1:455667379642:secret:gh-api-config-qa-E73Reo"
     current_api_image_tag = jsondecode(var.current_api_image_tag)["imageTags"][0]
 }
 
