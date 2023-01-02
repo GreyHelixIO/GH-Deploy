91a7ce99a9cdf46ba6f27fe3d4ff6008185c7856
Christopher Smith <csmith4work@gmail.com>
2023-01-02 16:55:33 -0600

    moving over tf files.

---
+++ tf/global/gh-api-pipeline-builder.tf
@@ -0,0 +1,67 @@
+terraform {
+    backend "s3" {
+        bucket = "gh-terraform"
+        key = "	gh-terraform-statefile.tfstate"
+        region = "us-east-1"
+    }
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.19.0"
+        }
+    }
+
+    required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+    region  = "us-east-1"
+    access_key = var.AWS_ID
+    secret_key = var.AWS_SECRET
+}
+
+resource "aws_codebuild_project" "gh_api_pipeline_builder" {
+    name          = "gh-api-pipeline-builder"
+    description   = "CodeBuild project for building up the rest of the api CICD pipeline"
+    build_timeout = "5"
+    service_role  = var.aws_cicd_role_arn
+
+    artifacts {
+        type = "NO_ARTIFACTS"
+    }
+
+    environment {
+        compute_type                = "BUILD_GENERAL1_SMALL"
+        image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+        type                        = "LINUX_CONTAINER"
+        image_pull_credentials_type = "CODEBUILD"
+    }
+
+    source {
+        type            = "GITHUB"
+        location        = "https://github.com/GreyHelixIO/GHApi.git"
+        git_clone_depth = 1
+        buildspec = "./buildspec/build-pipeline.yaml"
+        git_submodules_config {
+            fetch_submodules = true
+        }
+    }
+
+    source_version = "aws-migration"
+}
+
+resource "aws_codebuild_source_credential" "gh-github-credentials" {
+    auth_type   = "PERSONAL_ACCESS_TOKEN"
+    server_type = "GITHUB"
+    token       = var.github_token
+}
+
+resource "aws_s3_bucket" "codepipeline_bucket" {
+    bucket = "gh-pipeline-bucket"
+}
+
+resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
+    bucket = aws_s3_bucket.codepipeline_bucket.id
+    acl    = "private"
+}
---
+++ tf/global/variables.tf
@@ -0,0 +1,42 @@
+variable "github_token" {
+    type = string
+    description = "Github access token."
+}
+
+variable "aws_cicd_role_arn" {
+    type = string
+    description = "The arn connected to the aws role for the ci-cd pipeline."
+    default = "arn:aws:iam::455667379642:role/gh-codebuild-access"
+}
+
+variable "gh_api_port" {
+    type = number
+    description = "Default port for the api to listen."
+    default = 80
+}
+variable "repo_owner" {
+    type        = string
+    default     = "GreyHelixIO"
+}
+variable "repo" {
+    type        = string
+    default     = "GHApi"
+}
+variable "branch" {
+    type        = string
+    default     = "aws-migration"
+}
+variable "poll_for_changes" {
+    type        = string
+    default     = true
+}
+
+variable "AWS_ID" {
+    type        = string
+    description = "AWS authentication ID."
+}
+
+variable "AWS_SECRET" {
+    type        = string
+    description = "AWS authentication secret."
+}
