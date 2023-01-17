abfb6b0f0cb21e2cb634c21a87742de68e7a082f
Christopher Smith <csmith4work@gmail.com>
2023-01-17 16:39:07 -0600

    Save state.

--- aws/secretsmanager/main.tf
+++ aws/secretsmanager/main.tf
@@ -5,16 +5,16 @@ data "aws_secretsmanager_secret_version" "gh_global_github_token_secret_version"
     secret_id = data.aws_secretsmanager_secret.gh_global_github_token.id
 }
 
-data "aws_secretsmanager_secret" "prod_build_secrets" {
+data "aws_secretsmanager_secret" "prod_api_build_secrets" {
     arn = "arn:aws:secretsmanager:us-east-1:455667379642:secret:gh-api-config-prod-wvQqls"
 }
-data "aws_secretsmanager_secret_version" "prod_build_secret_version" {
-    secret_id = data.aws_secretsmanager_secret.prod_build_secrets.id
+data "aws_secretsmanager_secret_version" "prod_api_build_secret_version" {
+    secret_id = data.aws_secretsmanager_secret.prod_api_build_secrets.id
 }
 
-data "aws_secretsmanager_secret" "qa_build_secret" {
+data "aws_secretsmanager_secret" "qa_api_build_secret" {
     arn = "arn:aws:secretsmanager:us-east-1:455667379642:secret:gh-api-config-qa-E73Reo"
 }
-data "aws_secretsmanager_secret_version" "qa_build_secret_version" {
-    secret_id = data.aws_secretsmanager_secret.qa_build_secret.id
+data "aws_secretsmanager_secret_version" "qa_api_build_secret_version" {
+    secret_id = data.aws_secretsmanager_secret.qa_api_build_secret.id
 }
--- aws/secretsmanager/outputs.tf
+++ aws/secretsmanager/outputs.tf
@@ -1,3 +1,4 @@
 output "GITHUB_TOKEN" {
     value = "${jsondecode(data.aws_secretsmanager_secret_version.gh_global_github_token_secret_version.secret_string)["GITHUB_TOKEN"]}"
 }
+
