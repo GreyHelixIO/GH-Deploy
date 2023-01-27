6bde4389e438acd2b7df7a4534eba1d7befbdd24
Christopher Smith <cryptochrisjames@CCJMBP.local>
2023-01-27 13:04:49 -0600

    Refactored alb and put it into a module.

--- aws/ecs/main.tf
+++ aws/ecs/main.tf
@@ -46,10 +46,6 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_cluster" "gh_cluster" {
-    name = "gh-cluster-${var.env}"
-}
-
 resource "aws_ecs_service" "gh_service" {
     name            = "gh-${var.service}-service-${var.env}"
     cluster         = aws_ecs_cluster.gh_cluster.id
@@ -59,85 +55,19 @@ resource "aws_ecs_service" "gh_service" {
     deployment_minimum_healthy_percent = 0
     force_new_deployment = true
     network_configuration {
-        subnets = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+        subnets = ["${module.sg.aws_default_subnet.default_subnet_a.id}", "${module.sg.aws_default_subnet.default_subnet_b.id}"]
         assign_public_ip = true
-        security_groups  = ["${aws_security_group.service_security_group.id}"]
+        security_groups  = ["${module.sg.aws_security_group.service_security_group.id}"]
     }
     load_balancer {
-        target_group_arn = "${aws_lb_target_group.target_group.arn}"
+        target_group_arn = "${var.alb_target_group_arn}"
         container_name   = "${aws_ecs_task_definition.gh_task_definition.family}"
         container_port   = 80
     }
 }
 
-resource "aws_alb" "application_load_balancer" {
-    name               = "gh-alb-${var.env}"
-    load_balancer_type = "application"
-    subnets = [
-        "${aws_default_subnet.default_subnet_a.id}",
-        "${aws_default_subnet.default_subnet_b.id}"
-    ]
-
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-}
-resource "aws_security_group" "load_balancer_security_group" {
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-resource "aws_security_group" "service_security_group" {
-    ingress {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-resource "aws_lb_target_group" "target_group" {
-    name        = "gh-target-group-${var.env}"
-    port        = 80
-    protocol    = "HTTP"
-    target_type = "ip"
-    vpc_id      = "${aws_default_vpc.default_vpc.id}"
-    health_check {
-        matcher = "200,301,302"
-        path = "/health"
-        interval = 60
-    }
-}
-
-resource "aws_lb_listener" "listener" {
-    load_balancer_arn = "${aws_alb.application_load_balancer.arn}"
-    port              = "80"
-    protocol          = "HTTP"
-    default_action {
-        type             = "forward"
-        target_group_arn = "${aws_lb_target_group.target_group.arn}"
-    }
+resource "aws_ecs_cluster" "gh_cluster" {
+    name = "gh-cluster-${var.env}"
 }
 
 resource "aws_cloudwatch_log_group" "log-group" {
@@ -149,16 +79,6 @@ resource "aws_cloudwatch_log_group" "log-group" {
     }
 }
 
-resource "aws_default_vpc" "default_vpc" {
-    tags = {
-        Name = "Default VPC"
-    }
-}
-
-resource "aws_default_subnet" "default_subnet_a" {
-    availability_zone = "us-east-1a"
-}
-
-resource "aws_default_subnet" "default_subnet_b" {
-    availability_zone = "us-east-1b"
+module "sg" {
+    source = "../securitygroup"
 }
--- aws/ecs/variables.tf
+++ aws/ecs/variables.tf
@@ -22,3 +22,8 @@ variable "env_vars" {
     type = list(map(string))
     description = "The list of environment variables to be provided for the task definition."
 }
+
+variable "alb_target_group_arn" {
+    type = string
+    description = "The target group arn of the ALB that the service will connect to."
+}
