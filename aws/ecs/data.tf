ebf04f7807eb5a03e4cb8d318d11f484d9ec4dca
Christopher Smith <cryptochrisjames@CCJMBP.local>
2023-02-25 02:35:44 -0600

    typos.

--- aws/ecs/data.tf
+++ aws/ecs/data.tf
@@ -10,6 +10,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 data "aws_ecs_service" "gh_service" {
-    cluster = aws_ecs_cluster.gh_cluster.id
-    service = aws_ecs_service.gh_service.name
+    cluster_arn = aws_ecs_cluster.gh_cluster.arn
+    service_name = aws_ecs_service.gh_service.name
 }
