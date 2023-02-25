7c4ed6c7f6a2e2ce640dbc8804c178a8f73b32c2
Christopher Smith <cryptochrisjames@CCJMBP.local>
2023-02-25 02:31:02 -0600

    Declaring data.

--- aws/ecs/data.tf
+++ aws/ecs/data.tf
@@ -8,3 +8,8 @@ data "aws_iam_policy_document" "assume_role_policy" {
         }
     }
 }
+
+data "aws_ecs_service" "gh_service" {
+    cluster = aws_ecs_cluster.gh_cluster.id
+    service = aws_ecs_service.gh_service.name
+}
